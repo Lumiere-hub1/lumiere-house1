1dfdb58112b08f79fc4c7597e1a0a524d8dcd74d
goalmind7-sudo <goalmind7@gmail.com>
2026-08-25 15:24:35 +0000

    Make Express entrypoint detectable by Vercel

--- server.ts
+++ server.ts
@@ -1,3 +1,7 @@
+import express from "express";
 import app from "./server/app";
+
+// Keep the direct Express import in this root entrypoint for Vercel's detector.
+void express;
 
 export default app;
